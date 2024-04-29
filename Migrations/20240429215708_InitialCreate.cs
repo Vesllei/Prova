c26dc076086a0e1d3815b3850dfb96142bdc4830
Vesllei <163066288+Vesllei@users.noreply.github.com>
2024-04-29 20:46:26 -0300

    Add files via upload

---
+++ Migrations/20240429215708_InitialCreate.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Weslley.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "funcionarios",
+                columns: table => new
+                {
+                    funcionarioId = table.Column<string>(type: "TEXT", nullable: false),
+                    Nome = table.Column<string>(type: "TEXT", nullable: true),
+                    Cpf = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_funcionarios", x => x.funcionarioId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "folhas",
+                columns: table => new
+                {
+                    folhaId = table.Column<string>(type: "TEXT", nullable: false),
+                    valor = table.Column<double>(type: "REAL", nullable: false),
+                    quantidade = table.Column<double>(type: "REAL", nullable: false),
+                    mes = table.Column<int>(type: "INTEGER", nullable: false),
+                    ano = table.Column<int>(type: "INTEGER", nullable: false),
+                    funcionarioId = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_folhas", x => x.folhaId);
+                    table.ForeignKey(
+                        name: "FK_folhas_funcionarios_funcionarioId",
+                        column: x => x.funcionarioId,
+                        principalTable: "funcionarios",
+                        principalColumn: "funcionarioId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_folhas_funcionarioId",
+                table: "folhas",
+                column: "funcionarioId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "folhas");
+
+            migrationBuilder.DropTable(
+                name: "funcionarios");
+        }
+    }
+}
---
+++ Models/AppdbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace Weslley;
+
+public class AppdbContext : DbContext
+{
+
+    public DbSet<Folha> Folhas { get; set; }
+    public DbSet<Funcionario> Funcionarios { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source = weslley_nicolas.db");
+
+
+    }
+}
